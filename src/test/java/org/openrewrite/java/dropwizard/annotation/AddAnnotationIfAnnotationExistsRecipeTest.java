fa2a905ab355df4f6b705475137c27d7007f6479
Tim te Beek <tim@moderne.io>
2025-03-17 11:16:47 +0100

    Use explicit classpath

--- src/test/java/org/openrewrite/java/dropwizard/annotation/AddAnnotationIfAnnotationExistsRecipeTest.java
+++ src/test/java/org/openrewrite/java/dropwizard/annotation/AddAnnotationIfAnnotationExistsRecipeTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 the original author or authors.
+ * Copyright 2025 the original author or authors.
  * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,7 +27,7 @@ class AddAnnotationIfAnnotationExistsRecipeTest implements RewriteTest {
 
     @Override
     public void defaults(RecipeSpec spec) {
-        spec.parser(JavaParser.fromJavaVersion().classpath(JavaParser.runtimeClasspath()));
+        spec.parser(JavaParser.fromJavaVersion().classpath("javax.persistence-api"));
     }
 
     @DocumentExample
