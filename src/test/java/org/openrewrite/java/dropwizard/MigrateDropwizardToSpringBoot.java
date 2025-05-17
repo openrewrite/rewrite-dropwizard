fe1238ff21f1150d43e2dd204260de22dc7d672e
Tim te Beek <tim@moderne.io>
2025-05-17 14:24:41 +0200

    Limit where we create new properties

--- src/test/java/org/openrewrite/java/dropwizard/MigrateDropwizardToSpringBoot.java
+++ src/test/java/org/openrewrite/java/dropwizard/MigrateDropwizardToSpringBoot.java
@@ -15,6 +15,7 @@
  */
 package org.openrewrite.java.dropwizard;
 
+import org.intellij.lang.annotations.Language;
 import org.junit.jupiter.api.Test;
 import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
@@ -23,72 +24,59 @@ import static org.openrewrite.properties.Assertions.properties;
 
 class MigrateDropwizardToSpringBoot implements RewriteTest {
 
+    @Language("properties")
+    private static final String EXPECTED = """
+      management.endpoints.web.exposure.include=*
+      management.health.livenessstate.enabled=true
+      management.health.readinessstate.enabled=true
+      management.server.port=8081
+      server.port=8080
+      spring.application.name=my-application
+      spring.datasource.driverClassName=org.h2.Driver
+      spring.datasource.url=jdbc:h2:mem:mydb
+      spring.datasource.username=sa
+      spring.jersey.application-path=/api
+      spring.jersey.type=servlet
+      """;
+
     @Override
     public void defaults(RecipeSpec spec) {
         spec.recipeFromResources("org.openrewrite.java.dropwizard.MigrateDropwizardToSpringBoot");
     }
 
     @Test
-    void run() {
-        // Validates yaml recipe
+    void createNewProperties() {
         rewriteRun( // TODO We likely should not unconditionally create these files with these values!
           //language=properties
           properties(
             null,
-            """
-            management.endpoint.health.probes.enabled=true
-            management.endpoint.health.show-components=ALWAYS
-            management.endpoint.health.show-details=ALWAYS
-            management.endpoints.web.base-path=/
-            management.endpoints.web.exposure.include=*
-            management.health.livenessstate.enabled=true
-            management.health.readinessstate.enabled=true
-            management.server.base-path=/management
-            management.server.port=8081
-            server.port=8080
-            server.shutdown=graceful
-            spring.application.name=my-application
-            spring.datasource.driverClassName=org.h2.Driver
-            spring.datasource.password=org.h2.Driver
-            spring.datasource.url=jdbc:h2:mem:mydb
-            spring.datasource.username=org.h2.Driver
-            spring.jersey.application-path=/api
-            spring.jersey.type=servlet
-            spring.jpa.hibernate.ddl-auto=validate
-            spring.jpa.hibernate.naming.physical-strategy=org.hibernate.boot.model.naming.PhysicalNamingStrategyStandardImpl
-            spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.H2Dialect
-            spring.jpa.show-sql=true
-            spring.main.allow-circular-references=true
-            """,
-            spec -> spec.path("src/main/resources/application.properties")),
+            EXPECTED,
+            spec -> spec.path("src/main/resources/application.properties"))
+        );
+    }
+
+    @Test
+    void updateExistingProperties() {
+        rewriteRun(
+          //language=properties
+          properties(
+            "foo=bar",
+            "foo=bar\n" + EXPECTED,
+            spec -> spec.path("src/main/resources/application.properties"))
+        );
+    }
+
+    @Test
+    void doNotChangeTestProperties() {
+        rewriteRun(
           //language=properties
           properties(
             null,
-            """
-              management.endpoint.health.probes.enabled=true
-              management.endpoint.health.show-components=ALWAYS
-              management.endpoint.health.show-details=ALWAYS
-              management.endpoints.web.base-path=/
-              management.endpoints.web.exposure.include=*
-              management.health.livenessstate.enabled=true
-              management.health.readinessstate.enabled=true
-              management.server.base-path=/management
-              management.server.port=8081
-              server.port=8080
-              server.shutdown=graceful
-              spring.application.name=my-application
-              spring.datasource.driverClassName=org.h2.Driver
-              spring.datasource.password=org.h2.Driver
-              spring.datasource.url=jdbc:h2:mem:mydb
-              spring.datasource.username=org.h2.Driver
-              spring.jersey.application-path=/api
-              spring.jersey.type=servlet
-              spring.jpa.hibernate.ddl-auto=validate
-              spring.jpa.hibernate.naming.physical-strategy=org.hibernate.boot.model.naming.PhysicalNamingStrategyStandardImpl
-              spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.H2Dialect
-              spring.jpa.show-sql=true
-              spring.main.allow-circular-references=true
-              """,
+            EXPECTED,
+            spec -> spec.path("src/main/resources/application.properties")),
+          //language=properties
+          properties(
+            "foo=bar",
             spec -> spec.path("src/test/resources/application.properties"))
         );
     }
